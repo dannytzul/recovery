7407bd18b2d6851161a23d1e96ea3691879014d7
ricardomartinez9568 <rm3@mscpsharks.org>
2019-07-26 19:08:12 -0700

    commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { async } from 'q';
 
 @Component({
   selector: 'app-tab2',
@@ -8,5 +7,30 @@ import { async } from 'q';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  constructor(public alertController: AlertController) {}
+  constructor(public alertCtrl: AlertController) {}
+
+  async presentAlertConfirm() {
+    console.log('click works');
+    const alert = await this.alertCtrl.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
